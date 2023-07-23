6062be77b2761ea0cf307eb17481185d095b5471
zevezev <zev@upsolve.org>
2023-07-23 00:28:30 -0400

    added better menu and started rsvp for sturgeon crit

--- src/App.tsx
+++ src/App.tsx
@@ -6,90 +6,137 @@ import { StrawberryMoon } from "./StrawberryMoon";
 import ValeImage from "./images/vale.jpeg";
 import styled from "styled-components";
 import { BuckMoon } from "./BuckMoon";
+import { SturgeonMoon } from "./SturgeonMoon";
+type SelectedMoon = "strawberry" | "flowering" | "buck" | "sturgeon";
 
-type SelectedMoon = "strawberry" | "flowering" | "buck";
-const moonMap: { [key in SelectedMoon]: ReactNode } = {
+const moonMap: { [key: string]: ReactNode } = {
   strawberry: <StrawberryMoon />,
   flowering: <FloweringMoon />,
   buck: <BuckMoon />,
+  sturgeon: <SturgeonMoon />,
 };
 function App() {
-  const [moon, setMoon] = useState<SelectedMoon>("buck");
+  const [moon, setMoon] = useState<SelectedMoon>("sturgeon");
+  const moonHasBackdrop = !!["flowering", "strawberry"].find(
+    (inList) => moon === inList
+  );
   return (
     <div className="App">
       <header className="App-header">
         <Backdrop selectedMoon={moon}>
-          <div
-            style={{
-              maxWidth: "600px",
-              display: "flex",
-              flexDirection: "column",
-              gap: "16px",
-              backgroundColor: "rgba(15, 31, 29, 0.7)",
-              padding: "20px",
-              borderRadius: "16px",
-              textAlign: "start",
-              margin: "30px",
-            }}
-          >
+          <MoonMenu moon={moon} setMoon={setMoon} />
+          <BodyBackground showBackdrop={moonHasBackdrop}>
             {moonMap[moon]}
-          </div>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              gap: "16px",
-              padding: "0px 20px 20px 20px",
-              overflow: "hidden",
-              flexWrap: "wrap",
-            }}
-          >
-            <MoonSelectButton
-              selected={moon === "flowering"}
-              style={{ background: "rgb(230, 242, 136,.7)", color: "black" }}
-              onClick={() => setMoon("flowering")}
-            >
-              May: Flowering Moon
-            </MoonSelectButton>
-            <MoonSelectButton
-              selected={moon === "strawberry"}
-              style={{ background: "rgba(239,39,39,.7)" }}
-              onClick={() => setMoon("strawberry")}
-            >
-              June: Strawberry Moon
-            </MoonSelectButton>
-            <MoonSelectButton
-              selected={moon === "buck"}
-              style={{ background: "black" }}
-              onClick={() => setMoon("buck")}
-            >
-              July: Buck Moon
-            </MoonSelectButton>
-          </div>
+          </BodyBackground>
         </Backdrop>
       </header>
     </div>
   );
 }
 
+type BodyBackgroundProps = {
+  showBackdrop: boolean;
+};
+const BodyBackground = styled.div<BodyBackgroundProps>`
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  background-color: ${(props) =>
+    props.showBackdrop ? "rgba(15, 31, 29, 0.7)" : ""};
+  padding: 20px;
+  border-radius: 16px;
+  text-align: start;
+  margin: 30px;
+`;
+type MoonMenuProps = {
+  moon: SelectedMoon;
+  setMoon: (moon: SelectedMoon) => void;
+};
+const MoonMenu: React.FC<MoonMenuProps> = ({
+  moon: selectedMoon,
+  setMoon: setSelectedMoon,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const onClickMoonButton = (clickedMoon: SelectedMoon) => {
+    if (isOpen) {
+      setSelectedMoon(clickedMoon);
+      setIsOpen(false);
+    } else {
+      setIsOpen(true);
+    }
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        gap: "20px",
+        width: "100%",
+        padding: "80px 0px 80px 0px",
+        overflow: "hidden",
+        flexWrap: "wrap",
+      }}
+    >
+      <MoonSelectButton
+        show={selectedMoon === "flowering" || isOpen}
+        selected={selectedMoon === "flowering"}
+        background="#f3ff95"
+        onClick={() => onClickMoonButton("flowering")}
+      >
+        Flowering Moon
+      </MoonSelectButton>
+      <MoonSelectButton
+        show={selectedMoon === "strawberry" || isOpen}
+        selected={selectedMoon === "strawberry"}
+        background="#ff9090"
+        onClick={() => onClickMoonButton("strawberry")}
+      >
+        Strawberry Moon
+      </MoonSelectButton>
+      <MoonSelectButton
+        show={selectedMoon === "buck" || isOpen}
+        selected={selectedMoon === "buck"}
+        background={"#ffa684"}
+        onClick={() => onClickMoonButton("buck")}
+      >
+        Buck Moon
+      </MoonSelectButton>
+      <MoonSelectButton
+        show={selectedMoon === "sturgeon" || isOpen}
+        selected={selectedMoon === "sturgeon"}
+        background={"#d3e6f6"}
+        onClick={() => onClickMoonButton("sturgeon")}
+      >
+        Sturgeon Moon
+      </MoonSelectButton>
+    </div>
+  );
+};
 type MoonSelectProps = {
-  selected?: boolean;
-  background?: string;
-  color?: string;
+  selected: boolean;
+  background: string;
+  show: boolean;
 };
 const MoonSelectButton = styled.div<MoonSelectProps>`
-  background: rgba(12, 25, 23, 0.7);
-  padding: 16px;
-  width: 90px;
+  background: ${(props) => props.background};
+  color: #3b134b;
+  font-size: 18px;
+  font-weight: 800;
+  padding: 20px;
+  width: 100px;
+  height: 100px;
   justify-content: center;
   align-items: center;
-  display: flex;
-  border-radius: 40px;
+  display: ${(props) => (props.show ? "flex" : "none")};
+  border-radius: 50%;
+  box-shadow: ${(props) =>
+    props.selected ? `0px 0px 40px 40px ` + props.background + "dd" : ""};
   border: ${(props) =>
     props.selected ? `3px solid white` : "3px solid transparent"};
   cursor: pointer;
-  :hover {
-    background: "rgba(12, 25, 23)";
+  &:hover {
+    box-shadow: 0px 0px 40px 40px ${(props) => props.background}ee;
   }
 `;
 
@@ -97,6 +144,7 @@ const moonCSS: { [key in SelectedMoon]: string } = {
   strawberry: ` background-color: #f3658b;`,
   flowering: `background-image: url(${ValeImage});`,
   buck: `background-color: black;`,
+  sturgeon: ` background-color: #1a0540;`,
 };
 
 type BackdropProps = {
---
+++ src/SturgeonMoon.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import styled from "styled-components";
+export const SturgeonMoon = () => {
+  const [email, setEmail] = useState("");
+  const onChangeEmail = (email: string) => {
+    setEmail(email);
+  };
+  const onSubmitEmail = () => {
+    //TODO with database: send email to that email, enter the email into my db.
+    console.log(email);
+  };
+  return (
+    <Strawberry>
+      <div>
+        In honor of the Sturgeon Moon, we're doing an online crit. This crit is
+        open to any artist with a work to share.
+      </div>
+      <div>
+        Enter your email:{" "}
+        <input
+          type="email"
+          onChange={(e) => onChangeEmail(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") onSubmitEmail();
+          }}
+        ></input>
+        <button onClick={onSubmitEmail}>RSVP</button>
+      </div>
+      <div>Contact</div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <a
+          href="https://www.instagram.com/fullmoonartfestival/"
+          target="_blank"
+        >
+          Instagram
+        </a>
+        fullmoonartfestival@gmail.com <div>410-599-2336</div>
+      </div>
+      <a
+        href="https://docs.google.com/forms/d/e/1FAIpQLScWur-y7b9uSxZs4a2ZtlbhSuJ5FSjG-tEGRkRg2jHkHEtFog/viewform?usp=sf_link"
+        target="_blank"
+      >
+        Join our email list
+      </a>
+      <div>
+        The Full Moon Art Festival is an ongoing project by&nbsp;
+        <a href="https://www.zevizenberg.com/" target="_blank">
+          Zev Izenberg
+        </a>
+        , a painter, software engineer, and collaborative artist based in
+        Brooklyn, and their growing community. The Buck Moon is the 6th Full
+        Moon Art Festival out of 12.
+      </div>
+      <div>Reach them at zevizenberg@gmail.com</div>
+    </Strawberry>
+  );
+};
+const Strawberry = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  a {
+    color: rgb(238, 147, 171);
+  }
+`;
